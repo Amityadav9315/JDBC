f92c94f2a4d4902d06c665e2d8d21c5acb71a8d5
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-04 08:50:39 +0530

    FirstJDBC

--- src/FirstJDBC.java
+++ src/FirstJDBC.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FirstJDBC {
+
+import java.sql.*;
+public class FirstJDBC {
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.jdbc.driver");
+            //creating connection
+            String url="jdbc :mysql :// localhost :3306 /youtube";
+            String username="root";
+            String password="root";
+            Connection con=DriverManager.getConnection(url,username,password);
+            if(con.isClosed()){
+                System.out.println("connection is closed");
+            }
+            else{
+                System.out.println("Connection created");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
