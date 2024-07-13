b1073cbbaabedc4d004616d7f03285bf1138be71
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-13 12:41:37 +0530

    CreateTable

--- src/CreateTable.java
+++ src/CreateTable.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class FirstJDBC {
+public class CreateTable {
     public static void main(String[] args) {
         try {
             // Load the JDBC driver
